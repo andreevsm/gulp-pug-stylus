d180952e1fcdddfd11913b439d8a49c2f44239e5
Sergey Andreev <sergey-andreev-97-97@mail.ru>
2017-09-26 23:06:48 +0300

    Finish creating charts (without refactoring)

--- source/blocks/content/content.js
+++ source/blocks/content/content.js
@@ -1,232 +1,169 @@
-    var myChart = document.getElementById('myChart').getContext('2d');
-    // Global Options
-    Chart.defaults.global.defaultFontFamily = "'Lato', sans-serif";
-    Chart.defaults.global.defaultFontSize = 14;
-    Chart.defaults.global.defaultFontColor = '#428DCA';
-    Chart.defaults.global.animation.duration = 4000;
-    // Chart.defaults.doughnut.cutoutPercentage = 50
-    // Chart.defaults.global.elements.doughnut.cutoutPercentage = 50;
+/**
+ * Global options for Chart
+ */
+Chart.defaults.global.defaultFontFamily = "'Lato', sans-serif";
+Chart.defaults.global.defaultFontSize = 14;
+Chart.defaults.global.defaultFontColor = '#428DCA';
+Chart.defaults.global.animation.duration = 4000;
 
-    var massPopChart = new Chart(myChart, {
-        type: 'line', // bar, horizontalBar, pie, line, doughnut, radar, polarArea
-        data: {
-            labels: ['1-4', '6-4', '11-4', '16-4', '21-4', '26-4'],
-            datasets: [{
-                label: 'Population',
-                data: [
-                    594,
-                    1945,
-                    960,
-                    1419,
-                    262,
-                    172
-                ],
-                //backgroundColor:'green',
-                backgroundColor: [
-                    'rgba(59, 170, 255, 0.3)'
-                ],
-                borderWidth: 0.1,
-                borderColor: '#428DCA',
-                hoverBorderWidth: 3,
-                hoverBorderColor: '#000'
-            }, {
-                label: 'Population',
-                data: [
-                    594,
-                    1045,
-                    2060,
-                    519,
-                    162,
-                    2072
-                ],
+var _totalSalesChart = document.getElementById('totalSales').getContext('2d');
+var _newUsers = document.getElementById('newUsers').getContext('2d');
+var _orderBreakdown = document.getElementById('orderBreakdown').getContext('2d');
+var _couponUsage = document.getElementById('couponUsage').getContext('2d');
 
-                //backgroundColor:'green',
-
-                backgroundColor: [
-
-                    'rgba(88, 181, 252, 1)',
-
-                ],
-
-                borderWidth: 2,
-
-                borderColor: '# 777 ',
-                hoverBorderWidth: 3,
-                hoverBorderColor: '#000'
-            }]
+var totalSalesChart = new Chart(_totalSalesChart, {
+    type: 'line',
+    data: {
+        labels: ['1-4', '6-4', '11-4', '16-4', '21-4', '26-4'],
+        datasets: [{
+            label: 'Total Sales',
+            data: [
+                594,
+                1945,
+                960,
+                1419,
+                262,
+                172
+            ],
+            backgroundColor: 'rgba(59, 170, 255, 0.3)',
+            borderWidth: 0.1,
+            borderColor: '#428DCA',
+            hoverBorderWidth: 1,
+            hoverBorderColor: '#000'
+        }, {
+            label: 'Total Sales',
+            data: [
+                594,
+                1045,
+                2060,
+                519,
+                162,
+                2072
+            ],
+            backgroundColor: 'rgba(88, 181, 252, 1)',
+            borderWidth: 0.1,
+            borderColor: '#777',
+            hoverBorderWidth: 1,
+            hoverBorderColor: '#000'
+        }]
+    },
+    options: {
+        legend: {
+            display: false
         },
-        options: {
-            // title: {
-            //     display: true,
-            //     text: 'TOTAL SALES',
-            //     fontSize: 25,
-            //     position: 'top'
-            // },
-
-            legend: {
-                display: true,
-                // position: 'right',
-                labels: {
-                    fontColor: '#fff',
-                    defaultFontFamily: "'Helvetica Neue', 'Helvetica', 'Arial', sans-serif",
-                    defaultFontSize: "normal"
-                }
-            },
-            layout: {
-                padding: {
-                    left: 0,
-                    right: 0,
-                    bottom: 0,
-                    top: 0
-                }
-            },
-            tooltips: {
-                enabled: true
-            }
+        tooltips: {
+            enabled: true
         }
-    });
+    }
+});
 
-
-
-    var myChart2 = document.getElementById('myChart2').getContext('2d');
-    // Global Options
-
-    var massPopChart2 = new Chart(myChart2, {
-        type: 'line', // bar, horizontalBar, pie, line, doughnut, radar, polarArea
-        data: {
-            labels: ['1-4', '6-4', '11-4', '16-4', '21-4', '26-4'],
-            datasets: [{
-                label: 'Population',
-                data: [
-                    594,
-                    1945,
-                    960,
-                    1419,
-                    262,
-                    172
-                ],
-                //backgroundColor:'green',
-                backgroundColor: [
-                    'rgba(59, 170, 255, 0.3)'
-                ],
-                borderWidth: 0.1,
-                borderColor: '#428DCA',
-                hoverBorderWidth: 3,
-                hoverBorderColor: '#000'
-            }, {
-                label: 'Population',
-                data: [
-                    594,
-                    1045,
-                    2060,
-                    519,
-                    162,
-                    2072
-                ],
-
-                //backgroundColor:'green',
-
-                backgroundColor: [
-
-                    'rgba(121, 101, 241, 1)',
-
-                ],
-
-                borderWidth: 2,
-
-                borderColor: '#777 ',
-                hoverBorderWidth: 3,
-                hoverBorderColor: '#000'
-            }]
+var newUsersChart = new Chart(_newUsers, {
+    type: 'line',
+    data: {
+        labels: ['1-4', '6-4', '11-4', '16-4', '21-4', '26-4'],
+        datasets: [{
+            label: 'New Users',
+            data: [
+                594,
+                945,
+                960,
+                1419,
+                1762,
+                172
+            ],
+            backgroundColor: 'rgba(59, 170, 255, 0.3)',
+            borderWidth: 0.1,
+            borderColor: '#428DCA',
+            hoverBorderWidth: 3,
+            hoverBorderColor: '#000'
+        }, {
+            label: 'New Users',
+            data: [
+                594,
+                1045,
+                2060,
+                519,
+                162,
+                372
+            ],
+            backgroundColor: 'rgba(121, 101, 241, 1)',
+            borderWidth: 0.1,
+            borderColor: '#777',
+            hoverBorderWidth: 1,
+            hoverBorderColor: '#000'
+        }]
+    },
+    options: {
+        legend: {
+            display: false
         },
-        options: {
-            // title: {
-            //     display: true,
-            //     text: 'TOTAL SALES',
-            //     fontSize: 25,
-            //     position: 'top'
-            // },
-
-            legend: {
-                display: true,
-                // position: 'right',
-                labels: {
-                    fontColor: '#fff',
-                    defaultFontFamily: "'Helvetica Neue', 'Helvetica', 'Arial', sans-serif",
-                    defaultFontSize: "normal"
-                }
-            },
-            layout: {
-                padding: {
-                    left: 0,
-                    right: 0,
-                    bottom: 0,
-                    top: 0
-                }
-            },
-            tooltips: {
-                enabled: true
-            }
+        tooltips: {
+            enabled: true
         }
-    });
+    }
+});
 
+var orderBreakdownChart = new Chart(_orderBreakdown, {
+    type: 'doughnut',
+    data: {
+        labels: ['60% lunch', '40% Dinner'],
+        datasets: [{
+            label: 'Total Order',
+            data: [400, 600],
+            backgroundColor: [
+                'rgba(254, 200, 108, 1)',
+                'rgba(239,102,117, 1)'
+            ],
+            borderWidth: 0.1,
+            borderColor: '#fff',
+            hoverBorderWidth: 1,
+            hoverBorderColor: '#000',
+        }]
+    },
+    options: {
+        cutoutPercentage: 75,
+        legend: {
+            display: true
+        }
+    }
+});
 
-
-
-    var myChart3 = document.getElementById('myChart3').getContext('2d');
-    // Global Opt
-    var massPopChart3 = new Chart(myChart3, {
-        labelAlign: 'center',
-        type: 'doughnut', // bar, horizontalBar, pie, line, doughnut, radar, polarArea
-        data: {
-            labels: ['60% lunch', '40% Dinner'],
-            datasets: [{
-                label: 'Population',
-                data: [
-                    400,
-                    600
-                ],
-                //backgroundColor:'green',
-                backgroundColor: [
-                    'rgba(254, 200, 108, 1)',
-                    'rgba(239,102,117, 1)'
-                ],
-                borderWidth: 0.1,
-                borderColor: '#fff',
-                hoverBorderWidth: 1,
-                hoverBorderColor: '#000'
-            }]
+var couponUsageChart = new Chart(_couponUsage, {
+    type: 'bar',
+    data: {
+        labels: ['65% Mon', '82% Tue', '87% Wed', '23% Thr', '68% Fri', '27% Sat', '65% Sun'],
+        datasets: [{
+            label: 'Population',
+            data: [
+                650,
+                820,
+                870,
+                230,
+                680,
+                270,
+                650
+            ],
+            backgroundColor: [
+                'rgba(107, 202, 85, 1)',
+                'rgba(107, 202, 85, 1)',
+                'rgba(107, 202, 85, 1)',
+                'rgba(107, 202, 85, 1)',
+                'rgba(107, 202, 85, 1)',
+                'rgba(107, 202, 85, 1)',
+                'rgba(107, 202, 85, 1)'
+            ],
+            borderWidth: 0.1,
+            borderColor: '#428DCA',
+            hoverBorderWidth: 3,
+            hoverBorderColor: '#000'
+        }]
+    },
+    options: {
+        legend: {
+            display: false
         },
-        options: {
-            elements: {
-                center: {
-                    text: '50%',
-                }
-            },
-            cutoutPercentage: 75,
-
-            legend: {
-                display: false
-            }
+        tooltips: {
+            enabled: true
         }
-    });
-
-    Chart.pluginService.register({
-        beforeDraw: function(chart) {
-            var width = chart.chart.width,
-                height = chart.chart.height,
-                ctx = chart.chart.ctx;
-
-            ctx.restore();
-            var fontSize = (height / 114).toFixed(2);
-            ctx.font = fontSize + "em sans-serif";
-            ctx.textBaseline = "middle";
-
-            var text = "75%",
-                textX = Math.round((width - ctx.measureText(text).width) / 2),
-                textY = height / 2;
-
-            ctx.fillText(text, textX, textY);
-            ctx.save();
-        }
-    });
+    }
+});
